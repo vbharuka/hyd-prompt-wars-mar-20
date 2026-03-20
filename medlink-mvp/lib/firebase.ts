60fd8da9129ff4166649ba3811d36d28743d8d2e
Vaibhav Bharuka <vbharuka24@gmail.com>
2026-03-20 15:43:31 +0530

    feat: added google secret manager configuration

--- medlink-mvp/lib/firebase.ts
+++ medlink-mvp/lib/firebase.ts
@@ -4,10 +4,10 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Replace these with your project's Firebase configuration from the Firebase console
 const firebaseConfig = {
-  apiKey: "AIzaSyAeW68CmETCcHBI4PAcFThX2_xPxEE7SQA",
-  authDomain: `${process.env.NEXT_PUBLIC_GOOGLE_CLOUD_PROJECT || "prompt-wars-hyd-mar-20"}.firebaseapp.com`,
-  projectId: process.env.NEXT_PUBLIC_GOOGLE_CLOUD_PROJECT || "prompt-wars-hyd-mar-20",
-  storageBucket: `${process.env.NEXT_PUBLIC_GOOGLE_CLOUD_PROJECT || "prompt-wars-hyd-mar-20"}.firebasestorage.app`,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyAeW68CmETCcHBI4PAcFThX2_xPxEE7SQA",
+  authDomain: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "prompt-war-hyderabad"}.firebaseapp.com`,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "prompt-war-hyderabad",
+  storageBucket: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "prompt-war-hyderabad"}.firebasestorage.app`,
   messagingSenderId: "545506175753",
   appId: "1:545506175753:web:5eaa740e446306a3e541d8",
   measurementId: "G-J65NWPNK41"
